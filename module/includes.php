613def5f2cbb920cf3ee2d313e88adec49560968
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-24 11:27:34 +0900

    init fixture for wp_is_mobile (wp4.9 later)

--- module/includes.php
+++ module/includes.php
@@ -17,8 +17,9 @@ if ( c3_is_later_than_php_55() ) {
 if ( ! class_exists( 'CF_preview_fix' ) ) {
 	require_once( 'model/cf-preview-fix.php' );
 }
+	require_once( 'model/cf-preview-fix.php' );
 
-require_once( 'model/class.logs.php' );
+require_once( 'model/fixtures/wp_is_mobile.php' );
 
 // View
 require_once( 'view/components.php' );
