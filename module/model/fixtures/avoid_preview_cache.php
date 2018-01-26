97d3eee53dcc2eddcc7ae0207fa4c563bef6631a
hideokamoto <hide.okamoto@digitalcube.jp>
2018-01-26 13:26:49 +0900

    add deactivation hook

--- module/model/fixtures/avoid_preview_cache.php
+++ module/model/fixtures/avoid_preview_cache.php
@@ -9,11 +9,11 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @since 5.1.0
  * @access public
  */
-add_action( 'plugins_loaded', function(){
+add_action( 'plugins_loaded', function() {
 	if ( is_user_logged_in() ) {
 		setcookie( 'wordpress_loginuser_last_visit', time() );
 	}
-});
+} );
 
 
 /**
@@ -21,6 +21,20 @@ add_action( 'plugins_loaded', function(){
  *
  * @since 5.1.0
  */
-add_action( 'wp_logout', function(){
+add_action( 'wp_logout', 'c3_unset_avoid_cache_cookie' );
+
+/**
+ * Unet cookie for avoid CloudFront cache when user deactivate the plugin
+ *
+ * @since 5.1.0
+ */
+register_deactivation_hook( C3_PLUGIN_ROOT, 'c3_unset_avoid_cache_cookie' );
+
+/**
+ * Function unet cookie for avoid CloudFront cache when user sign out
+ *
+ * @since 5.1.0
+ */
+function c3_unset_avoid_cache_cookie() {
 	setcookie('wordpress_loginuser_last_visit', '', time() - 1800);
-});
+}
