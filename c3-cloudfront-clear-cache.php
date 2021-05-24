dbdec0c574f20a1dcda2977b6c7db3dd70e65d42
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-24 17:48:07 +0900

    chore: loader

--- c3-cloudfront-clear-cache.php
+++ c3-cloudfront-clear-cache.php
@@ -11,15 +11,10 @@
  * @package c3-cloudfront-clear-cache
  */
 
-require_once( __DIR__ . '/classes/Class_Loader.php' );
 define( 'C3_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'C3_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'C3_PLUGIN_ROOT', __FILE__ );
-
-new C3_CloudFront_Cache_Controller\Class_Loader( dirname( __FILE__ ) . '/classes' );
-new C3_CloudFront_Cache_Controller\Class_Loader( dirname( __FILE__ ) . '/classes/WP' );
-new C3_CloudFront_Cache_Controller\Class_Loader( dirname( __FILE__ ) . '/classes/AWS' );
-new C3_CloudFront_Cache_Controller\Class_Loader( dirname( __FILE__ ) . '/classes/Views' );
+require_once( __DIR__ . '/loader.php' );
 
 use C3_CloudFront_Cache_Controller\WP;
 use C3_CloudFront_Cache_Controller\AWS;
