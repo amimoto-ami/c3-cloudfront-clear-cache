b415fe7841c75a7c1c56d6c5d67a33e5c6a17fef
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-31 12:37:35 +0900

    local build cmd

--- bin/build.sh
+++ bin/build.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 set -e
-rm -f .gitignore
+git checkout release
+git merge master
+rm -rf .github node_modules/ vendor/ .distignore .editorconfig .phpcs.xml.dist .wp-env.json phpunix.xml tests/ renovate.json
+git add ./
+git commit -m "release package"
+git push origin release -f
