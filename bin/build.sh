b4ec19a106287d467134f030fc06149a1fbdb833
Hidetaka Okamoto <info@wp-kyoto.net>
2021-05-31 12:38:38 +0900

    chore: update local build cmd

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 set -e
+git branch -D release
 git checkout release
-git merge master
 rm -rf .github node_modules/ vendor/ .distignore .editorconfig .phpcs.xml.dist .wp-env.json phpunix.xml tests/ renovate.json
 git add ./
 git commit -m "release package"
